136b5af4c0c17fc3255d0a4ccb95a79d327b142f
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-06-22 19:02:43 -0400

    test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,13 +1,16 @@
-// const Player = require('../lib/Player');
+const Player = require('../lib/Player');
 const Potion = require('../lib/Potion.js');
-jest.mock('../lib/Potion.js');
+jest.mock('../lib/Potion');
 console.log(new Potion());
 
-// test('creates a player object', () => {
-//   const player = new Player('Dave');
+test('creates a player object', () => {
+  const player = new Player('Dave');
 
-//   expect(player.name).toBe('Dave');
-//   expect(player.health).toEqual(expect.any(Number));
-//   expect(player.strength).toEqual(expect.any(Number));
-//   expect(player.agility).toEqual(expect.any(Number));
-// });
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+});
